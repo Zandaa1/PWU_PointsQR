68b5878dbd6d2ec387d9551a8d8b1235fc7c313d
Zandaa <91167131+Zandaa1@users.noreply.github.com>
2025-03-27 22:16:52 +0800

    fix commit

--- logout.php
+++ logout.php
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 <?php
    session_start();
 
    if(session_destroy()) {
       header("Location: index.php");
    }
-=======
-<?php
-   session_start();
-
-   if(session_destroy()) {
-      header("Location: index.php");
-   }
->>>>>>> 5d788cbc96d3c2ba7cdc639f31545429c816a353
 ?>
