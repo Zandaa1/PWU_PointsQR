5d788cbc96d3c2ba7cdc639f31545429c816a353
Alex "Zandaa" Mangahas <91167131+Zandaa1@users.noreply.github.com>
2025-03-27 21:07:16 +0800

    initial upload
    things to do next
    
    - include 1st year login page
    - remove create qr code
    - reset to 0 points/reset user data (points, history)
    - remove selection faculty
    - all guest

---
+++ dbqrscan.sql
@@ -0,0 +1,156 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 27, 2025 at 01:59 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbqrscan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `qr_codes`
+--
+
+CREATE TABLE `qr_codes` (
+  `id` int(6) UNSIGNED NOT NULL,
+  `unique_text` varchar(255) NOT NULL,
+  `booth_name` varchar(50) NOT NULL,
+  `description` text DEFAULT NULL,
+  `points` int(6) NOT NULL,
+  `expiration_time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `qr_codes`
+--
+
+INSERT INTO `qr_codes` (`id`, `unique_text`, `booth_name`, `description`, `points`, `expiration_time`, `created_at`) VALUES
+(1, 'IOKSA', 'IT Booth', 'Visiting the booth.', 1, '2025-12-14 04:00:00', '2025-03-12 14:43:59'),
+(4, 'IOKSW', 'IT Booth', 'Bought Sticker', 5, '2025-03-14 05:33:00', '2025-03-13 05:33:51'),
+(5, 'MNY5T', 'Sample Booth', '+50 visited booth', 50, '2025-03-27 16:00:00', '2025-03-27 12:25:56');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `scan_history`
+--
+
+CREATE TABLE `scan_history` (
+  `id` int(6) UNSIGNED NOT NULL,
+  `user_id` int(6) UNSIGNED NOT NULL,
+  `qr_code_id` int(6) UNSIGNED NOT NULL,
+  `scan_time` timestamp NOT NULL DEFAULT current_timestamp(),
+  `points_added` int(6) NOT NULL,
+  `description` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `scan_history`
+--
+
+INSERT INTO `scan_history` (`id`, `user_id`, `qr_code_id`, `scan_time`, `points_added`, `description`) VALUES
+(8, 1, 1, '2025-03-13 05:33:18', 1, NULL),
+(9, 1, 4, '2025-03-13 05:34:03', 5, NULL),
+(10, 1, 5, '2025-03-27 12:26:41', 50, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(6) UNSIGNED NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('student','faculty','admin') NOT NULL,
+  `points` int(6) UNSIGNED DEFAULT 0,
+  `reg_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `role`, `points`, `reg_date`) VALUES
+(1, 'a1550023', '1234', 'student', 56, '2025-03-27 12:26:41');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `qr_codes`
+--
+ALTER TABLE `qr_codes`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `unique_text` (`unique_text`);
+
+--
+-- Indexes for table `scan_history`
+--
+ALTER TABLE `scan_history`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `qr_code_id` (`qr_code_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `qr_codes`
+--
+ALTER TABLE `qr_codes`
+  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `scan_history`
+--
+ALTER TABLE `scan_history`
+  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `scan_history`
+--
+ALTER TABLE `scan_history`
+  ADD CONSTRAINT `scan_history_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `scan_history_ibfk_2` FOREIGN KEY (`qr_code_id`) REFERENCES `qr_codes` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PWU Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
+        <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+  />
+    <style>
+        body {
+            background-image: url(pwu_bg.jpg);
+            background-size: cover;
+            background-position: center;
+            margin: 0;
+            padding: 0;
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        h1,
+        p {
+            color: white;
+            text-align: center;
+            margin: 0;
+        }
+
+        h1 {
+            font-family: Montserrat, sans-serif;
+            font-weight: 600;
+        }
+
+        p {
+            font-family: Montserrat;
+        }
+
+        .main-page {
+            width: 90%;
+            max-width: 400px;
+            text-align: center;
+        }
+
+        .button-container {
+            margin-top: 20px;
+        }
+
+        .btn {
+            width: 100%;
+            margin-bottom: 10px;
+        }
+
+    </style>
+
+
+</head>
+
+<body>
+
+
+    <div class="container main-page">
+
+    
+        <p class="animate__animated animate__fadeIn">Welcome to</p>
+        
+        <img src="pwu_logo.png" width="75px" alt="">
+
+        <h1 class="animate__animated animate__zoomIn">PWU Points Tracker</h1>
+        
+        
+        <div class="button-container">
+            <a href="student_login.php" class="btn btn-danger">
+                Student/Faculty
+            </a>
+
+            <a href="admin_login.php" class="btn btn-danger">
+                Administrator
+            </a>
+        </div>
+
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
